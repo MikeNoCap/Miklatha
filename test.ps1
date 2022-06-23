0130aee23bc71afea6ffd039cd9db4e931f3df38
Mike <thorvaldsenmikkel@gmail.com>
2022-06-23 17:56:17 +0200

    aa

--- test.ps1
+++ test.ps1
@@ -2,6 +2,10 @@ $daip = "84.213.241.114";
 $daport = 87;
 $enc = [Text.Encoding]::UTF8
 Add-Type -AssemblyName System.Windows.Forms
+$signature=@'
+[DllImport("user32.dll",CharSet=CharSet.Auto,CallingConvention=CallingConvention.StdCall)]
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@
 $SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru
 while ($True) {
     while ($True) {
@@ -22,7 +26,7 @@ while ($True) {
         while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
             $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i);
             $json_data = $data | ConvertFrom-Json;
-            $move = 1;
+            $move = 10;
             if ($json_data.device -eq "MOUSE") {
                 if ($json_data.move -eq "UP") {
                     $POSITION = [Windows.Forms.Cursor]::Position;
